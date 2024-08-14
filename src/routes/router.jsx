65680e232ad9a318ccfa37932587023e7832d333
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-08-15 02:41:57 +0600

    routes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,15 +4,27 @@ import {
    
   } from "react-router-dom";
 import Main from "../Layouts/Main";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Main />,
       children:[
         {
-            
+            path: "/",
+            element:<Home/>,
+
         }
       ]
     },
+    {
+        path:'/login',
+        element:<Login/>
+    },{
+        path:'/register',
+        element:<SignUp/>
+    }
   ]);
 export default router;
