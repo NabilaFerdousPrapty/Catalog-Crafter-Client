8e6552fd7994bf9240379409ccca01cdac21f735
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-08-16 04:42:01 +0600

    products added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -14,7 +14,9 @@ const Home = () => {
                 <div className="lg:max-w-lg">
                     <h1 className="text-3xl font-semibold text-gray-800 lg:text-4xl">Best place to choose <br /> your <span className="text-blue-500 ">Electronics</span></h1>
                     
-                    <p className="mt-3 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
+                    <p className="mt-3 text-gray-600 dark:text-gray-400">
+                        The best place to buy electronics online. We offer a wide range of products including laptops, mobile phones, tablets, cameras, and more. Shop now and get the best deals on electronics.
+                    </p>
                     
                     <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Shop Now</button>
                 </div>
